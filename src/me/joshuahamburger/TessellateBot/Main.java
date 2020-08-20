6e0f6d522b92ed8135f90411c1337f65b7737892
HamburgJ <joshdhamburger@gmail.com>
2020-08-19 22:04:19 -0400

    Fixed token

--- src/me/joshuahamburger/TessellateBot/Main.java
+++ src/me/joshuahamburger/TessellateBot/Main.java
@@ -1,5 +1,9 @@
 package me.joshuahamburger.TessellateBot;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
@@ -11,7 +15,19 @@ public class Main {
 	public static String prefix = "!";
 	
 	public static void main(String[] args) throws LoginException {
-		String token = "NzQ0Mjg5NjQyOTQwOTg5NTAw.XzhDvw.5i-6dIUlDX1EI45EhMIC8weOuQ0";
+		String token = null;
+        try {
+            File tokenFile = Paths.get("token.txt").toFile();
+            if (!tokenFile.exists()) {
+                System.out.println("[ERROR] Could not find token.txt file");
+                System.out.println("[ERROR] Please create a file called \"token.txt\" in the same folder as the jar " + "file and paste in your bot token.");
+                return;
+            }
+            token = new String(Files.readAllBytes(tokenFile.toPath()));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if (token == null) return;
 		jda = JDABuilder.createDefault(token).build();
 		
 		jda.getPresence().setStatus(OnlineStatus.ONLINE);
